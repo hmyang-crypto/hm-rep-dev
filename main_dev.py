3ae96c6b98b3ebef9080d12b3327539a5ddd6728
hmyang-crypto <hm.yang@dohands.com>
2026-09-07 19:41:46 +0900

    Update main_dev.py

--- main_dev.py
+++ main_dev.py
@@ -13,7 +13,7 @@ from datetime import datetime, timedelta
 from functools import partial
 
 # 💡 테스트 전용 GitHub Raw 주소
-UPDATE_CHECK_URL = "https://raw.githubusercontent.com/hmyang-crypto/hm-rep/refs/heads/main/version_dev.txt"
+UPDATE_CHECK_URL = "https://raw.githubusercontent.com/hmyang-crypto/hm-rep-dev/refs/heads/main/version_dev.txt"
 UPDATE_CODE_URL = "https://raw.githubusercontent.com/hmyang-crypto/hm-rep-dev/refs/heads/main/main_dev.py"
 CURRENT_VERSION = "2.1.0"
 
